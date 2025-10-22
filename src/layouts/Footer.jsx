2063dcf72face0e18dbf543f1e51e3efd8ecf648
Parsa Abedi <learnforwork2022@gmail.com>
2025-10-22 22:21:54 +0330

    Creating a footer

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>Developed by Parsa with ❤️</p>
+    </footer>
+  );
+}
+
+export default Footer;
