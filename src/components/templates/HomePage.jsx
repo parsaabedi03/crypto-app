81be6689cbf8a5abda8fa1350723435dc2273c6a
Parsa Abedi <learnforwork2022@gmail.com>
2025-10-23 22:46:15 +0330

    Home page component with three states for getting data

---
+++ src/components/templates/HomePage.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+import TableCoin from "../modules/TableCoin";
+import { getCoinList } from "../../services/cryptoApi";
+import SesrchBar from "../modules/SesrchBar";
+import Pagination from "../modules/Pagination";
+
+function HomePage() {
+  const [coins, setCoins] = useState([]);
+  const [currency, setCurrency] = useState("usd");
+  const [paginate, setPaginate] = useState(1);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { url, options } = getCoinList(currency, paginate);
+      const response = await fetch(url, options);
+      const json = await response.json();
+      setCoins(json);
+    };
+    getData();
+  }, [currency, paginate]);
+
+  return (
+    <div>
+      <SesrchBar currency={currency} setCurrency={setCurrency} />
+      <TableCoin coins={coins} currency={currency} />
+      <Pagination paginate={paginate} setPaginate={setPaginate} />
+    </div>
+  );
+}
+
+export default HomePage;
