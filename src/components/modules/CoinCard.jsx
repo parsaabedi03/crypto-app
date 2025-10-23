8faff95cfc19dee940d99ccef312c66a778f56e7
Parsa Abedi <learnforwork2022@gmail.com>
2025-10-23 22:52:50 +0330

    Coin Card component to display information about each coin

---
+++ src/components/modules/CoinCard.jsx
@@ -0,0 +1,50 @@
+import styles from "./TableCoin.module.css";
+
+import churtUp from "../../assets/chart-up.svg";
+import churtDown from "../../assets/chart-down.svg";
+
+function CoinCard({ data, currency }) {
+  const {
+    image,
+    symbol,
+    name,
+    current_price,
+    price_change_percentage_24h,
+    total_volume,
+  } = data;
+
+  const icon = {
+    usd: "$",
+    eur: "€",
+    jpy: "¥",
+  };
+
+  return (
+    <tr className={styles.card}>
+      <td className={styles.coin}>
+        <img src={image} alt={name} />
+        <span>{symbol.toUpperCase()}</span>
+      </td>
+      <td>{name}</td>
+      <td>
+        {icon[currency]} {current_price.toLocaleString()}
+      </td>
+      <td
+        className={0 <= price_change_percentage_24h ? styles.green : styles.red}
+      >
+        {price_change_percentage_24h.toFixed(2)} %
+      </td>
+      <td>
+        {icon[currency]} {total_volume.toLocaleString()}
+      </td>
+      <td>
+        <img
+          src={0 <= price_change_percentage_24h ? churtUp : churtDown}
+          alt="chart"
+        />
+      </td>
+    </tr>
+  );
+}
+
+export default CoinCard;
