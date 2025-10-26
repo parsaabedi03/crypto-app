e5890948fb81b86afdd3f13a889702ab019636c6
Parsa Abedi <learnforwork2022@gmail.com>
2025-10-26 20:54:47 +0330

    Creating a modal component to display a market cap and total volume price chart

--- src/components/modules/ModalChart.jsx
+++ src/components/modules/ModalChart.jsx
@@ -1,17 +1,84 @@
-import { useEffect } from "react";
-import { getChartData } from "../../services/cryptoApi";
+import { useEffect, useState } from "react";
 
-function ModalChart() {
+import Chart from "./Chart";
+
+import { getChartData } from "../../services/cryptoApi";
+import { formatChartData } from "../../utils/formatChartData";
+
+import styles from "./ModalChart.module.css";
+
+function ModalChart({
+  setShowModal,
+  currency,
+  coinData: { id, name, image, ath, current_price, market_cap },
+}) {
+  const [dataChart, setDataChart] = useState([]);
+  const [selectedMetric, setSelectedMetric] = useState("prices");
+
+  const formattedData = formatChartData(
+    dataChart[selectedMetric] || [],
+    selectedMetric
+  );
+
+  const metrics = [
+    { key: "prices", label: "Prices" },
+    { key: "market_caps", label: "Market Caps" },
+    { key: "total_volumes", label: "Total Volumes" },
+  ];
+
+  const icon = {
+    usd: "$",
+    eur: "€",
+    jpy: "¥",
+  };
+
   useEffect(() => {
-    const getChart = async () => {
-      const { url, options } = getChartData("bitcoin");
+    const getChart = async (id) => {
+      const { url, options } = getChartData(id);
       const response = await fetch(url, options);
       const json = await response.json();
-      console.log(json);
+      setDataChart(json);
     };
-    getChart();
-  }, []);
-  return <div>ModalChart</div>;
+    getChart(id);
+  }, [id]);
+
+  return (
+    <div className={styles.container}>
+      <button className={styles.close} onClick={() => setShowModal(false)}>
+        X
+      </button>
+      <div className={styles.modal}>
+        <div className={styles.detail}>
+          <img src={image} alt={name} />
+          <p>{name}</p>
+        </div>
+        <Chart data={formattedData} metric={selectedMetric} />
+        {metrics.map(({ key, label }) => (
+          <button
+            key={key}
+            className={selectedMetric === key ? styles.selected : ""}
+            onClick={() => setSelectedMetric(key)}
+          >
+            {label}
+          </button>
+        ))}
+        <div className={styles.coinStats}>
+          <p>
+            <span>Price: </span>
+            {icon[currency]} {current_price?.toLocaleString()}
+          </p>
+          <p>
+            <span>Ath: </span>
+            {icon[currency]} {ath?.toLocaleString()}
+          </p>
+          <p>
+            <span>Market Cap: </span>
+            {icon[currency]} {market_cap?.toLocaleString()}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ModalChart;
